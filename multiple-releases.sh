632f51a261754abe13d8f87954c04335ec9a7c88
Niclas Söderström <niclas.soderstrom@gmp-systems.com>
2019-07-31 20:18:08 +0200

    Updated "multiple" script

--- multiple-releases.sh
+++ multiple-releases.sh
@@ -8,9 +8,10 @@ releasesWithOldCli=(
 2.0.2
 2.0.3
 3.0.0
-3.1.1
-3.1.2
-3.2.0
+#3.1.0 Can not be created with detox
+#3.1.1 Can not be created with detox
+#3.1.2 Can not be created with detox
+#3.2.0 Can not be created with detox
 )
 
 # with  yarn global add ignite-cli
