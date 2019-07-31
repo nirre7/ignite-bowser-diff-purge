b842bf2d0b0a63a0f798044680ea4b2be4fc7375
Niclas Söderström <niclas.soderstrom@gmp-systems.com>
2019-07-31 09:30:36 +0200

    Updated urls

--- generate-table.js
+++ generate-table.js
@@ -6,7 +6,7 @@ const semver = require('semver')
 const getStdin = require('get-stdin')
 
 
-const RepoUrl='https://github.com/react-native-community/rn-diff-purge'
+const RepoUrl='https://github.com/nirre7/ignite-diff-purge'
 
 
 const generateTable = async () => {
@@ -40,7 +40,7 @@ const generateTable = async () => {
                             return '-'
                         }
                         return isBig
-                            ? `[->${toRelease}](${RepoUrl}/compare/release/${fromRelease}..release/${toRelease}) [core](https://github.com/facebook/react-native/compare/v${fromRelease}...v${toRelease})`
+                            ? `[->${toRelease}](${RepoUrl}/compare/release/${fromRelease}..release/${toRelease}) [ignite-bowser core](https://github.com/infinitered/ignite-bowser/compare/v${fromRelease}...v${toRelease})`
                             : `[->${toRelease}](${RepoUrl}/compare/release/${fromRelease}..release/${toRelease})`
                     }),
                 )(R.range(0, length)),
